a334673343da459eac7305d85c2264a849228bdc
peanut <shuouyang0@gmail.com>
2023-11-30 11:47:43 +0800

    优化第76题的算法

--- src/main/java/leetcode/Solution_76.kt
+++ src/main/java/leetcode/Solution_76.kt
@@ -1,12 +1,14 @@
 package leetcode
 
 class Solution_76 {
-    //有优化的空间
+    //优化后的算法
     fun minWindow(s: String, t: String): String {
         val pointer = IntArray(128) //需求数组
         var char = ' '
+        var need = 0
         for (char in t) {
             pointer[char.code]-- //需要的
+            need --
         }
         var L = 0
         var R = 0
@@ -15,24 +17,20 @@ class Solution_76 {
         var end = sLen + 1
         while (R < sLen) {
             char = s[R]
+            if (pointer[char.code] < 0){ //找到了一个需要的
+                need ++
+            }
             pointer[char.code]++
-            var includeAll = true
-            while (includeAll) {
-                for (char in t) {
-                    if (pointer[char.code] < 0) {
-                        includeAll = false
-                        break
-                    }
+            while (need == 0) {
+                if (R - L < end - start) {
+                    start = L
+                    end = R
                 }
-                if (includeAll){
-                    if (R - L < end - start) {
-                        start = L
-                        end = R
-                    }
-                    pointer[s[L].code]--
-                    L++
+                if (pointer[s[L].code] == 0){ //排除的是一个需要的
+                    need -- //需要的又要欠下一个了
                 }
-
+                pointer[s[L].code]--
+                L++
             }
             R++
         }
